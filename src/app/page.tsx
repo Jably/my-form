7a19225d2ce52903b932f9966d566e652ca55594
Jably <trainzfans02@gmail.com>
2024-10-29 01:43:20 +0700

    Signed-off-by: Jably <trainzfans02@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     // Dynamically add Midtrans script
     const script = document.createElement('script');
     script.src = 'https://app.sandbox.midtrans.com/snap/snap.js';
-    script.setAttribute('data-client-key', process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY || '');
+    script.setAttribute('data-client-key', process.env.NEXT_PRIVATE_MIDTRANS_CLIENT_KEY || '');
     document.body.appendChild(script);
 
     // Cleanup
